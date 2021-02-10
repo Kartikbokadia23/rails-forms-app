d745343aee1b85afd0d8510be58a6fd58e481983
kartikbokadia23 <kartikbokadia@gmail.com>
2021-02-10 16:49:51 +0530

    added form using form_with

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
+
+    def show
+        @user = User.find(params[:id])
+      end
+
     def new
+        @user = User.new
     end
 
     def create
@@ -12,6 +18,19 @@ class UsersController < ApplicationController
             render :new
         end
     end
+
+    def edit
+        @user = User.find(params[:id])
+      end
+    
+      def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+          redirect_to user_path(@user)
+        else
+          render :edit
+        end
+      end
 end
 
 private 
